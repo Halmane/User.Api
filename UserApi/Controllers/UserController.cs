b4654cd4622c0c3cbbdfa1eef5ca89b6b3e72f13
Halmane <durmanovova@gmail.com>
2023-11-02 17:26:16 +0400

    fixed logging of method errors

--- UserApi/Controllers/UserController.cs
+++ UserApi/Controllers/UserController.cs
@@ -10,7 +10,7 @@ namespace UserApi.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    readonly ILogger<UserController> _logger;
+    private readonly ILogger<UserController> _logger;
     private readonly ApplicationContext _context;
 
     public UserController(ApplicationContext context, ILogger<UserController> logger)
@@ -22,70 +22,110 @@ public class UserController : ControllerBase
     [HttpGet("{id}")]
     public async Task<IResult> GetUserAsync(string id)
     {
-        using (ApplicationContext db = new ApplicationContext())
+        try
         {
-            var result = await db.GetUserAsync(id);
-            if (result != null)
-                return Results.Json(result);
-            _logger.LogError("GetUserAsync => User Not Found {@result}", result);
-            return Results.NotFound(new { message = "Not Found" });
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var result = await db.GetUserAsync(id);
+                if (result != null)
+                    return Results.Json(result);
+                _logger.LogWarning("{@MethodName} => User Not Found {@id}", nameof(GetUserAsync), id);
+                return Results.NotFound(new { message = "Not Found" });
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "an error occurred in method {@MethodName}", nameof(GetUserAsync));
+            return Results.StatusCode(500);
         }
     }
 
     [HttpGet("users/{page}")]
     public async Task<IResult> GetUsersAsync(int page = 0, int pageSize = 10)
     {
-        using (ApplicationContext db = new ApplicationContext())
+        try
         {
-            var result = await db.GetUsersAsync(page, pageSize);
-            if (result != null)
-                return Results.Json(result);
-            _logger.LogError("GetUsersAsync => User Not Found {@result}", result);
-            return Results.NotFound(new { message = "Not Found" });
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var result = await db.GetUsersAsync(page, pageSize);
+                if (result != null)
+                    return Results.Json(result);
+                _logger.LogWarning("{@MethodName} => Users Not Found {@result}", nameof(GetUsersAsync), result);
+                return Results.NotFound(new { message = "Not Found" });
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "an error occurred in method {@MethodName}", nameof(GetUsersAsync));
+            return Results.StatusCode(500);
         }
     }
 
     [HttpPost("create")]
     public async Task<IResult> PostUserAsync(User user)
     {
-        if (ModelState.IsValid)
+        try
         {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("{@MethodName} => User Incorrect data {@result}", nameof(PostUserAsync), user);
+                return Results.BadRequest(new { message = "Incorrect data" });
+            }
             user.Id = Guid.NewGuid().ToString();
             using (ApplicationContext db = new ApplicationContext())
             {
                 return Results.Json(await db.AddUserAsync(user));
             }
         }
-        _logger.LogError("PostUserAsync => User Incorrect data {@result}", user);
-        return Results.BadRequest(new { message = "Incorrect data" });
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "an error occurred in method {@MethodName}", nameof(PostUserAsync));
+            return Results.StatusCode(500);
+        }
     }
 
     [HttpPut("update")]
     public async Task<IResult> ChangeUserAsync(User userData)
     {
-        if (ModelState.IsValid)
-            using (ApplicationContext db = new ApplicationContext())
-            {
-                var result = await db.UpdateUserAsync(userData);
-                if (result != null)
-                    return Results.Json(result);
-                _logger.LogError("ChangeUserAsync => User Not Found {@result}", result);
-                return Results.NotFound(new { message = "Not Found" });
-            }
-        _logger.LogError("ChangeUserAsync => User Incorrect data {@result}", userData);
-        return Results.BadRequest(new { message = "Incorrect data" });
+        try
+        {
+            if (ModelState.IsValid)
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    var result = await db.UpdateUserAsync(userData);
+                    if (result != null)
+                        return Results.Json(result);
+                    _logger.LogError("ChangeUserAsync => User Not Found {@id}", userData.Id);
+                    return Results.NotFound(new { message = "Not Found" });
+                }
+            _logger.LogWarning("{@MethodName} => User Incorrect data {@result}", nameof(ChangeUserAsync), userData);
+            return Results.BadRequest(new { message = "Incorrect data" });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "an error occurred in method {@MethodName}", nameof(ChangeUserAsync));
+            return Results.StatusCode(500);
+        }
     }
 
     [HttpDelete("{id}/delete")]
     public async Task<IResult> DeleteUserAsync(string id)
     {
-        using (ApplicationContext db = new ApplicationContext())
+        try
         {
-            var result = await db.DeleteUserAsync(id);
-            if (result != null)
-                return Results.Json(result);
-            _logger.LogError("DeleteUserAsync => User Not Found {@result}", result);
-            return Results.NotFound(new { message = "Not Found" });
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var result = await db.DeleteUserAsync(id);
+                if (result != null)
+                    return Results.Json(result);
+                _logger.LogWarning("{@MethodName} => User Not Found {@id}", nameof(DeleteUserAsync), id);
+                return Results.NotFound(new { message = "Not Found" });
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "an error occurred in method {@MethodName}", nameof(DeleteUserAsync));
+            return Results.StatusCode(500);
         }
     }
 }
